fffd74ef64569e6374ad29f979aa9ca75c406ee6
Catalin Dinu <catalin@nets.ro>
2025-07-23 14:18:47 +0300

    Add vtr_status.js

---
+++ tests/vtr_status.js
@@ -0,0 +1,17 @@
+const { autoScanVtrs, getVtrStatus, VTR_PORTS } = require('./src/commands/vtr_interface');
+
+/**
+ * Check status of a specific VTR port
+ * @param {string} path - VTR port path
+ */
+async function checkSingleVtr(path) {
+  console.log(`\n🔍 Checking VTR at ${path}...`);
+  // filepath: check_vtr_status.js
+const { autoScanVtrs, getVtrStatus, VTR_PORTS } = require('./src/commands/vtr_interface');
+
+/**
+ * Check status of a specific VTR port
+ * @param {string} path - VTR port path
+ */
+async function checkSingleVtr(path) {
+  console.log(`\n🔍 Checking
